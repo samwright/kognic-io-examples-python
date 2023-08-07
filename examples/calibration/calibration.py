e5b7181e63aa670c9578cea40fa6bbc93854dd25
annotell <github-actions@github.com>
2023-08-07 14:04:53 +0000

    Merge pull request #20 from annotell/sp-721-principalpointfisheye
    Added PrincipalPointFisheyeCalibration

--- examples/calibration/calibration.py
+++ examples/calibration/calibration.py
@@ -6,13 +6,15 @@ import kognic.io.model.calibration as CalibrationModel
 from examples.calibration.create_kannala_calibration import unity_kannala_calibration
 from examples.calibration.create_fisheye_calibration import unity_fisheye_calibration
 from examples.calibration.create_pinhole_calibration import unity_pinhole_calibration
-from examples.calibration.create_principal_point_distortion_calibration import unity_principal_point_distortion_calibration
+from examples.calibration.create_principal_point_distortion_calibration import \
+    unity_principal_point_distortion_calibration
 from examples.calibration.create_lidar_calibration import unity_lidar_calibration
 from examples.calibration.create_cylindrical_calibration import example_cylindrical_calibration
 from examples.calibration.create_fused_cylindrical_calibration import example_fused_cylindrical_calibration
+from examples.calibration.create_principal_point_fisheye_calibration import unity_principal_point_fisheye_calibration
+
 
 def create_sensor_calibration(external_id, lidar_sources: List[str] = None, camera_sources: List[str] = None):
-
     if lidar_sources is None:
         lidar_sources = []
 
@@ -25,6 +27,7 @@ def create_sensor_calibration(external_id, lidar_sources: List[str] = None, came
         unity_pinhole_calibration(),
         unity_fisheye_calibration(),
         unity_principal_point_distortion_calibration(),
+        unity_principal_point_fisheye_calibration(),
         example_cylindrical_calibration(),
         example_fused_cylindrical_calibration(),
     ]
@@ -33,7 +36,8 @@ def create_sensor_calibration(external_id, lidar_sources: List[str] = None, came
         **{camera_source: camera_calibrations.pop() for camera_source in camera_sources}
     }
     calibration_external_id = external_id
-    sensor_calibration = CalibrationModel.SensorCalibration(external_id=calibration_external_id, calibration=calibration_dict)
+    sensor_calibration = CalibrationModel.SensorCalibration(external_id=calibration_external_id,
+                                                            calibration=calibration_dict)
 
     return sensor_calibration
 
---
+++ examples/calibration/create_principal_point_fisheye_calibration.py
@@ -0,0 +1,23 @@
+from kognic.io.model.calibration.camera.common import CameraMatrix
+from kognic.io.model.calibration.camera.principal_point_fisheye_calibration import (PrincipalPointFisheyeCalibration,
+                                                                                    PrincipalPointFisheyeCoefficients)
+from kognic.io.model.calibration.common import Position, RotationQuaternion
+
+
+def unity_principal_point_fisheye_calibration():
+    camera_position = Position(x=0.0, y=0.0, z=0.0)
+    camera_rotation = RotationQuaternion(w=1.0, x=0.0, y=0.0, z=0.0)
+    camera_camera_matrix = CameraMatrix(fx=3450, fy=3250, cx=622, cy=400)
+    principal_point_fisheye_coefficients = PrincipalPointFisheyeCoefficients(alpha_l=0.0,
+                                                                             alpha_r=0.0,
+                                                                             beta_u=0.0,
+                                                                             beta_l=0.0)
+    return PrincipalPointFisheyeCalibration(
+        position=camera_position,
+        rotation_quaternion=camera_rotation,
+        camera_matrix=camera_camera_matrix,
+        principal_point_fisheye_coefficients=principal_point_fisheye_coefficients,
+        image_height=1080,
+        image_width=1920,
+        field_of_view=180.0
+    )
