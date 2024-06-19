303d6447c733381da75618fafd69d6e1339b8609
annotell <github-actions@github.com>
2024-06-19 14:51:02 +0000

    Add review resource for new endpoints in shape integration api (#94)
    Co-authored-by: Oskar Olin <oskar.olin@annotell.com>

---
+++ examples/post_review.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+
+from kognic.io.client import KognicIOClient
+from kognic.io.logger import setup_logging
+from kognic.io.model.review.review import (
+    AddFeedbackItem,
+    AddFeedbackItemPin,
+    AddFeedbackItemSuggestedProperty,
+    ReviewRequest,
+    ReviewResponse,
+    ReviewWorkflowEnum,
+)
+
+
+def run(client: KognicIOClient, open_label_uuid: str, error_type_id: str) -> ReviewResponse:
+    review = ReviewRequest(
+        feedback_items=[
+            AddFeedbackItem(
+                sensor_id="<the id/name of the sensor>",
+                frame_id="<the id of the frame>",
+                object_id="<the id of the object>",
+                pin=AddFeedbackItemPin(x=0.0, y=0.0, z=0.0),
+                description="I post this via the Python client",
+                suggested_property=AddFeedbackItemSuggestedProperty(
+                    property_name="propertyName", suggested_property_value="suggestedPropertyValue"
+                ),
+                error_type_id=error_type_id,
+                metadata={"key": "value"},
+            )
+        ],
+        workflow=ReviewWorkflowEnum.CORRECT,
+        accepted=False,
+    )
+
+    return client.review.create_review(open_label_uuid=open_label_uuid, body=review)
+
+
+if __name__ == "__main__":
+    setup_logging(level="DEBUG")
+
+    client = KognicIOClient()
+
+    open_label_uuid = "<the uuid of the OpenLABEL>"
+    error_type_id = "<the id of the error type>"
+    annotation = run(client, open_label_uuid, error_type_id)
